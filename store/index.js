f3da47df83b3f7c674fa6b2c3e135eaea235fdb3
Artyom Aristov <dei40752@gmail.com>
2020-04-09 20:52:28 +0300

    Initial commit for Netlify 🚀

--- store/index.js
+++ store/index.js
@@ -83,3 +83,21 @@ export const state = () => ({
     }
   ]
 })
+
+export const mutations = {
+  setProjects (state, list) {
+    state.projects = list
+  }
+}
+
+export const actions = {
+  async nuxtServerInit ({ commit }) {
+    const files = await require.context('~/assets/content/blog/', false, /\.json$/)
+    const projects = files.keys().map((key) => {
+      const res = files(key)
+      res.slug = key.slice(2, -5)
+      return res
+    })
+    await commit('setProjects', projects)
+  }
+}
