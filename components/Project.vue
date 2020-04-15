c3675a8c36616714164dc34906b133d0b0682ab9
Artyom Aristov <dei40752@gmail.com>
2020-04-15 18:58:44 +0300

    stop click events from link and techs list

--- components/Project.vue
+++ components/Project.vue
@@ -1,25 +1,25 @@
 <template>
-  <li :class="`select-none rounded relative cursor-pointer transform transition-all relative duration-75 p-4 mb-5 pointer ${cardIsOpened ? 'shadow-2xl scale-105' : 'hover:shadow-2xl hover:scale-105'}`" @click.self="selectCard">
-    <h3 class="text-md leading-6 font-bold mb-4" @click="selectCard">
+  <li data-testid="project-container" :class="`select-none rounded relative cursor-pointer transform transition-all relative duration-75 p-4 mb-5 pointer ${cardIsOpened ? 'shadow-2xl scale-105' : 'hover:shadow-2xl hover:scale-105'}`" @click="selectCard">
+    <h3 class="text-md leading-6 font-bold mb-4">
       {{ project.title }}
     </h3>
     <!-- TODO: add sorting by techs later -->
-    <ul class="inline-flex mb-4">
+    <ul class="inline-flex mb-4" @click.stop>
       <li v-for="(icon, i) in project.stack" :key="i" class="mr-2">
         <img class="h-5" :src="`${icon}`" alt="">
       </li>
     </ul>
-    <div v-if="!cardIsOpened" @click="selectCard">
+    <div v-if="!cardIsOpened" data-testid="project-preview-of-insides">
       {{ project.description }}
     </div>
-    <div v-else @click.self="selectCard">
-      <div class="mb-4" @click="selectCard">
+    <div v-else data-testid="project-insides">
+      <div class="mb-4">
         {{ project.body }}
       </div>
-      <a target="_blank" class="text-gray-600 underline inline-block mb-4" :href="project.link">{{ project.link }}</a>
-      <img class="h-100" :src="`${project.img}`" alt="" srcset="" @click="selectCard">
+      <a target="_blank" class="text-gray-600 underline inline-block mb-4" :href="project.link" @click.stop>{{ project.link }}</a>
+      <img class="h-100" :src="`${project.img}`" alt="" srcset="">
     </div>
-    <button :class="`transform transition-transform duration-75 mt-4 block w-5 mx-auto opacity-25 ${cardIsOpened ? 'rotate-180' : ''}`" @click="selectCard">
+    <button :class="`transform transition-transform duration-75 mt-4 block w-5 mx-auto opacity-25 ${cardIsOpened ? 'rotate-180' : ''}`">
       <img src="~/assets/img/arrow.svg" alt="" srcset="">
     </button>
   </li>
