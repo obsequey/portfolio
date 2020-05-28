027c9c4be1d67128ed553a0684c35ab30327e1ec
Artyom Aristov <in@aristovdev.com>
2020-05-28 19:54:39 +0300

    add defer to netlify script

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@ export default {
   },
   head () {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js', defer: true }]
     }
   }
 }
